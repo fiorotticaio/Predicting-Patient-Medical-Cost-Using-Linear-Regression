d013ea041690dda076ae3e9be0d922f591525588
fiorotticaio <caiofiorotti@gmail.com>
2024-05-12 13:28:42 -0300

    :globe_with_meridians: translating comments to english

--- main.py
+++ main.py
@@ -3,41 +3,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import max_error, mean_absolute_error, root_mean_squared_error, mean_absolute_percentage_error, mean_squared_error, median_absolute_error, r2_score
 
-# Carregar os dados do arquivo CSV
+# Upload data from CSV file
 data = pd.read_csv("insurance.csv")
 
-# Remove os valores nulos
+# Remove null values
 data = data.dropna() 
 
-# Converter a coluna "sex" para valores numéricos (0 para male, 1 para female)
+# Convert the "sex" column to numeric values (0 for male, 1 for female)
 data['sex'] = data['sex'].map({'male': 0, 'female': 1})
 
-# Converter a coluna "smoker" para valores numéricos (0 para no, 1 para yes)
+# Convert "smoker" column to numeric values (0 for no, 1 for yes)
 data['smoker'] = data['smoker'].map({'no': 0, 'yes': 1})
 
-# Converter a coluna "region" para one-hot encoding
+# Convert the "region" column to one-hot encoding
 data = pd.get_dummies(data, columns=['region'])
 
-# Separar os dados em features (X) e target (y)
+# Separate data into features (X) and target (y)
 X = data.drop(columns=['charges'])
 y = data['charges']
 
-# Dividir os dados em conjuntos de treino e teste
+# Split data into training and testing sets (80% training, 20% testing)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Inicializar o modelo de regressão linear
+# Initialize the linear regression model
 model = LinearRegression()
 
-# Treinar o modelo
+# Train the model
 model.fit(X_train, y_train)
 
-# Fazer predições nos dados de teste
+# Make predictions on test data
 y_pred = model.predict(X_test)
 
-# Conferindo se y_pred e y_test possuem o mesmo tamanho
+# Checking if y_pred and y_test have the same size
 assert y_pred.shape == y_test.shape
 
-# Calcular métricas de avaliação
+# Calculate assessment metrics
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 rmse = root_mean_squared_error(y_test, y_pred)
@@ -46,7 +46,7 @@ r2 = r2_score(y_test, y_pred)
 median_ae = median_absolute_error(y_test, y_pred)
 max_err = max_error(y_test, y_pred)
 
-# Imprimir métricas
+# Print metrics
 print(f"Mean Absolute Error: {mae:.2f}")
 print(f"Mean Squared Error: {mse:.2f}")
 print(f"Root Mean Squared Error: {rmse:.2f}")
